f413e14d45d0338e8edbdaa0d3bc21cf2c22ea91
Mikhail Kuzmenkov <63548200+MikhailKuzm@users.noreply.github.com>
2022-07-04 12:05:10 +0300

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, Response
 app = Flask(__name__)   
 
 @app.route('/')
-def index():
-    print('works')
-    return render_template('./index.html')
+def index(): 
+    return 'works'
+    #return render_template('./index.html')
 
-@app.route("/video_feed") 
-def video_feed():
-    from Face_detect.launch_face_detection import func
-    return Response(func(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#@app.route("/video_feed") 
+#def video_feed():
+#    from Face_detect.launch_face_detection import func
+#    return Response(func(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
    app.run(debug=False)
