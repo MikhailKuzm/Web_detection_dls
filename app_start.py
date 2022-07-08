223f7c70f3192fb6baae942812f10cad16fe31b0
Mikhail Kuzmenkov <63548200+MikhailKuzm@users.noreply.github.com>
2022-07-08 12:10:07 +0300

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, Response
-
+import os
+os.chdir('C://Users//Mikhail//Desktop//repos//Web_detection')
 app = Flask(__name__)   
 
 @app.route('/')
-def index(): 
-    return 'works'
-    #return render_template('./index.html')
+def index():
+    return render_template('./index.html')
 
-#@app.route("/video_feed") 
-#def video_feed():
-#    from Face_detect.launch_face_detection import func
-#    return Response(func(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/video_feed") 
+def video_feed():
+    from Face_detect.launch_face_detection import func
+    return Response(func(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
    app.run(debug=False)
